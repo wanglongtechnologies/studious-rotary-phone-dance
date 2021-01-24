0ebaca0a77f15a085202d74ebf27d05685d466ab
wanglongtechnologies <68694404+wanglongtechnologies@users.noreply.github.com>
2021-01-24 18:48:43 +0800

    Create results.js

---
+++ results/results.js
@@ -0,0 +1,40 @@
+/** @format */
+
+let results = {
+  'Finals - Placings': {
+    'Group A - Ballroom Solo (Under 12 & Below - Waltz & Quickstep)': ['87', '107', '115', '135', '133', '41', '35', '123'],
+    'Group A - Ballroom Solo (Under 16 & Below - Tango & Foxtrot)': ['88', '108'],
+    'Group A - Ballroom Solo (Adults - Waltz & Tango)': ['139', '80'],
+
+    'Group A - Latin Solo (Under 12 & Below - Cha Cha & Jive)': ['67', '3', '145', '51', '1', '5', '89', '86'],
+    'Group A - Latin Solo (Under 16 & Below - Rumba & Samba)': ['148', '58', '4', '146', '79', '147', '50'],
+    'Group A - Latin Solo (Adults - Cha Cha & Rumba)': ['59', '63', '61', '138', '75', '33', '49'],
+
+    'Group A - Latin Synchronize (Under 12 & Below - Cha Cha & Jive)': ['83', '9', '136', '118', '27', '127'],
+    'Group A - Latin Synchronize (Under 16 & Below - Rumba & Samba)': ['48', '40'],
+
+    'Group C - Ballroom Solo (Under 8 & Below - Waltz & Quickstep)': ['101', '91', '97'],
+    'Group C - Ballroom Solo (Under 12 & Below - Waltz & Quickstep)': ['103', '99', '94', '126', '129'],
+    'Group C - Ballroom Solo (Adults - Waltz & Tango)': ['90'],
+
+    'Group C - Latin Solo (Under 8 & Below - Cha Cha & Jive)': ['64', '25', '32', '52', '105', '18', '116', '106', '92'],
+    'Group C - Latin Solo (Under 12 & Below - Cha Cha & Jive)': ['143', '124', '137', '24', '17', '77', '12'],
+    'Group C - Latin Solo (Under 16 & Below - Rumba & Samba)': ['144', '20', '141', '140', '84'],
+    'Group C - Latin Solo (Adults - Cha Cha & Rumba)': ['93'],
+
+    'Group C - Latin Synchronize (Under 8 & Below - Cha Cha & Jive)': ['29', '122', '121', '95'],
+    'Group C - Latin Synchronize (Under 12 & Below - Cha Cha & Jive)': ['28', '30', '16', '78', '109'],
+    'Group C - Latin Synchronize (Under 16 & Below - Rumba & Samba)': ['142'],
+  },
+  Finals: {
+    'Group A - Latin Solo (Under 12 and Below - Cha Cha & Jive)': ['1', '3', '5', '51', '67', '86', '89', '145'],
+    'Group A - Latin Solo (Under 16 and Below - Rumba & Samba)': ['4', '50', '58', '79', '146', '147', '148'],
+    'Group C - Latin Solo (Under 8 and Below - Cha Cha & Jive)': ['18', '25', '32', '52', '64', '92', '105', '106', '116'],
+    'Group C - Latin Solo (Under 12 and Below - Cha Cha & Jive)': ['12', '17', '24', '77', '124', '137', '143'],
+  },
+  'Semi Finals': {
+    'Group A - Latin Solo (Under 12 and Below - Cha Cha & Jive)': ['1', '3', '5', '8', '45', '51', '66', '67', '68', '70', '72', '81', '86', '89', '104', '145'],
+    'Group A - Latin Solo (Under 16 and Below - Rumba & Samba)': ['2', '4', '6', '50', '58', '60', '62', '69', '79', '146', '147', '148'],
+    'Group C - Latin Solo (Under 12 and Below - Cha Cha & Jive)': ['11', '12', '17', '19', '24', '54', '76', '77', '96', '112', '124', '137', '143'],
+  },
+};
